f3d4c960581e29e6710755bfc4c6494f2401a6ca
Ner0theHer0 <malachi.mashiah@students.mq.edu.au>
2020-02-14 14:59:40 +1100

    Role assigning now fully functional (and dynamic)

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import random
 
 from discord.ext import commands
+from discord.utils import get
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -38,10 +39,41 @@ async def coin_flip(ctx):
     await ctx.send(response)
 
 @bot.command(name='role', help='adds role if it exits. Use: !role Dota')
-async def dota_time(ctx, arg):
-    print ('this is supposed to give you role ' + arg)
+async def dota_time(ctx, *arg):
 
+    whitelist = (
+        'test',
+        'one two'
+    )
     
+    server = ctx.guild.id
+    sRoles = bot.get_guild(server).roles
+    
+    if (arg):
+        argStr = ' '.join(arg)
+        tst = 0
+        for x in range(len(sRoles)):
+            if sRoles[x].name == argStr:
+                tst = 1
+                if sRoles[x].name in whitelist:
+                    user = ctx.author
+                    if (sRoles[x] not in user.roles):
+                        await user.add_roles(sRoles[x])
+                        response = (user.mention + ' is now a member of ' + sRoles[x].name)
+                        await ctx.send(response)
+                    else:
+                        await user.remove_roles(sRoles[x])
+                        response = (user.mention + ' is no longer a member of ' + sRoles[x].name)
+                        await ctx.send(response)
+                else:
+                    await ctx.send("This role is not whitelisted")
+        if (x+1 >= len(sRoles) and tst == 0):
+            await ctx.send("This role does not exist")
+    else:
+        roleStr = " \n".join(whitelist)
+        await ctx.send(ctx.author.mention + ", use !role (role) to assign yourself a role.\n"
+                       "The following roles are self assignable:\n```"
+                       + roleStr + "```")
 
 bot.run(token)
 bot.users
