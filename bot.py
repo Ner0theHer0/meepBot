d42be35cd3645bca8ac588670bed1d6cfe3b7730
Ner0theHer0 <malachi.mashiah@students.mq.edu.au>
2020-02-14 17:47:49 +1100

    Added exit command that can be run as admin

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import random
+import sys
 
 from discord.ext import commands
 from discord.utils import get
@@ -55,6 +56,17 @@ async def coin_flip(ctx):
     ]
     response = random.choice(result)
     await ctx.send(response)
+
+@bot.command(name='exit', help='Sends the bot offline')
+async def go_offline(ctx):
+    if (ctx.author.guild_permissions.administrator):
+        response = "Goodnight!"
+        await ctx.send(response)
+        sys.exit(0)
+    else:
+        response = "Only an admin can perform this action"
+        await ctx.send(response)
+        
 
 @bot.command(name='git', help='Links to github repo')
 async def git_post(ctx):
