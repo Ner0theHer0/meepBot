22cbccdcf033885077d4d3054493d32510cd82e0
Ner0theHer0 <malachi.mashiah@students.mq.edu.au>
2020-02-17 18:30:06 +1100

    Functioning role/whitelist with cog classes

--- bot.py
+++ bot.py
@@ -1,47 +1,25 @@
+import discord
 from discord.ext import commands
-from discord.utils import get
-import os
+
 from dotenv import load_dotenv
+import os
+
+import sys
+import traceback
+
+initial_extensions = [
+    'cogs.role_tools'
+    #'cogs.members',
+    #'cogs.admin'
+]
+
+bot = commands.Bot(command_prefix='!', description="I bring the FUN in disfunction!")
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        bot.load_extension(extension)
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-description = 'Bringing the FUN in disfunction'
-
-default_extensions = ["role_tools", "misc"]
-
-bot = commands.Bot(command_prefix='!', description=description)
-
-@bot.event
-async def on_ready():
-    print('Logged in as ' +
-          str(bot.user.name) + " " +
-          str(bot.user.id))
-
-@bot.command(name="load", help="Loads an extention")
-async def load(ctx, extention : str):
-    try:
-        bot.load_extension(extension)
-    except (AttributeError, ImportError) as e:
-        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
-        return
-    await ctx.send("{} loaded.".format(extension))
-
-@bot.command(name="unload", help="Unloads an extention")
-async def unload(ctx, extension : str):
-    bot.unload_extension(extension)
-    await ctx.send("{} unloaded.".format(extension))
-
-@bot.command(name='add', help="Adds two numbers")
-async def add_smth(ctx, int1 : int, int2 : int):
-    await ctx.send(int1 + int2)
-
-if __name__ == "__main__":
-    for extension in default_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Failed to load extension {}\n{}'.format(extension, exc))
-
-    bot.run(token)
+bot.run(token)
--- cogs/role_tools.py
+++ cogs/role_tools.py
@@ -1,27 +1,39 @@
 #role_tools.py
 
-import discord
+import os
+import random
+import sys
+import csv
+
 from discord.ext import commands
+from discord.utils import get
+from dotenv import load_dotenv
+import discord
 
-class role_tools():
-    def init(self, bot):
+import csv
+
+
+class role_toolsCog(commands.Cog):
+
+    #bot = commands.Bot(command_prefix='!', description="I bring the FUN in disfunction!")
+
+    def __init__(self, bot):
         self.bot = bot
+    
+    @commands.command(name='role', help='adds role if it exits. Use: !role Dota', pass_context = True)
+    async def role_command(self, ctx, *arg):
 
-    @bot.command(name='role', help='adds role if it exits. Use: !role Dota', commands_heading='Roles')
-    async def dota_time(ctx, *arg):
+        whitelist = []
 
-        whitelist = (
-            'test',
-            'one two'
-        )
-        
-        server = ctx.guild.id
-        sRoles = bot.get_guild(server).roles
+        whitelist = read_csv('whitelist.csv')
+
+        #server = ctx.guild.id
+        sRoles = ctx.guild.roles
         
         if (arg):
             argStr = ' '.join(arg)
             for x in range(len(sRoles)):
-                if sRoles[x].name == argStr:
+                if sRoles[x].name == argStr.lower():
                     if sRoles[x].name in whitelist:
                         user = ctx.author
                         if (sRoles[x] not in user.roles):
@@ -47,5 +59,52 @@ class role_tools():
                            "The following roles are self assignable:\n```"
                            + roleStr + "```")
 
+    @commands.command(name='whitelist', help='whitelists a role')
+    async def add_to_whitelist(self, ctx, *arg):
+        
+        if not ctx.author.guild_permissions.administrator:
+            await ctx.send("Only and administrator can perform this action")
+            
+        else:
+            argStr = ' '.join(arg)
+            argStr.lower()
+            
+            whitelist = []
+            whitelist = read_csv('whitelist.csv')
+
+            with open('whitelist.csv', 'w', newline='') as wl:
+
+                server = ctx.guild.id
+                sRoles = ctx.guild.roles
+
+                for z in range(len(sRoles)):
+                    if sRoles[z].name.lower() in argStr:
+                        
+                        write = csv.writer(wl,  delimiter=' ')
+                        
+                        if argStr in whitelist:
+                            whitelist.remove(argStr)
+                            await ctx.send("Removed role '" + argStr + "' from whitelist")
+
+                        else:
+                            write.writerow([argStr])
+                            await ctx.send("Whitelisted role '" + argStr + "'")
+
+                        for x in range(len(whitelist)):
+                            write.writerow([whitelist[x]])
+                        return
+                await ctx.send("Role '" + argStr + "' does not exist")
+    
+def read_csv(filename):
+            with open(filename) as wl:
+                read = csv.reader(wl)
+
+                wlist = []
+                col = 0
+                for row in read:
+                    wlist.append(row[0])
+                return wlist
+            
 def setup(bot):
-    bot.add_cog(role_tools(bot))
+    bot.add_cog(role_toolsCog(bot))
+    
